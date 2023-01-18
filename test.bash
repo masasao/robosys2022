d10ab10b7f71c8a1f67b2dca1ad6d220d253cd47
masanao <ninjinuragosi@gmail.com>
2023-01-18 23:13:42 +0900

    test4

--- test.bash
+++ test.bash
@@ -10,6 +10,13 @@ res=0
 out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ] || ng $LINENO
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+        　 
+out=$(echo | ./plus) 
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
