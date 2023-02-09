4682de83a349d274591673626aa2d021386bbef3
masasao <ninjinuragosi@gmail.com>
2023-02-10 04:12:51 +0900

    real finish

--- test.bash
+++ test.bash
@@ -1,14 +1,17 @@
 #!/bin/bash 
 # SPDX-FileCopyrightText: 2023 Masanao Hayashi
 # SPDX-License-Identifier: BSD-3-Clause
-ng () {
-      	echo NG at Line $1
-        res=1
-}　
-res=0   
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-　
-[ "$res" = 0 ] && echo OK
-exit $res
+ng () {
+	      echo NG at Line $1
+	            res=1
+	    }
+	    　
+	    res=0
+	    　
+	   
+	    out=$(seq 5 | ./plus)
+	    [ "${out}" = 15 ] || ng ${LINENO}
+	    　
+	    [ "$res" = 0 ] && echo OK        
+	    exit $res
