ca1f7387b4c4f0a9619be5ef6ec0bdde5817d3a8
masasao <ninjinuragosi@gmail.com>
2023-02-12 13:01:20 +0900

    fixed test

--- test.bash
+++ test.bash
@@ -3,15 +3,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	      echo NG at Line $1
-	            res=1
-	    }
-	    　
-	    res=0
-	    　
-	   
-	    out=$(seq 5 | ./plus)
-	    [ "${out}" = 15 ] || ng ${LINENO}
-	    　
-	    [ "$res" = 0 ] && echo OK        
-	    exit $res
+	        echo NG at Line $1
+        	res=1
+}
+
+res=0
++ res=0
+
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+
